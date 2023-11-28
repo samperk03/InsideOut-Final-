c9b4c3abbbdf45c944c322a66f8fe703f7d27e1f
jtlung <jtlung01@gmail.com>
2023-11-28 12:35:27 -0500

    Player
    added player sprite and basic movement

---
+++ Player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+const speed = 300.0
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
