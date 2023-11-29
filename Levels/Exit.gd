4475d0ae8179119cffcc45f39dfd6c5b3d653e4e
greysonschenck69 <greysonschenck@yahoo.com>
2023-11-29 18:13:00 -0500

    Update

---
+++ Levels/Exit.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		if name == "Exit_to_1":
+			var _target = get_tree().change_scene_to_file("res://Levels/Level1.tscn")
+		if name == "Exit_to_2":
+			var _target = get_tree().change_scene_to_file("res://Levels/Level2.tscn")
+		if name == "Exit_to_GO":
+			var _target = get_tree().change_scene_to_file("res://Levels/Game_Over.tscn")
